ff7f9e86eff060255f356cdd179cfe4521a5309b
KevH98 <har_kev@hotmail.com>
2022-05-04 23:46:10 +0100

    code for employee_training to filter and get the procudure docs to assign for training

---
+++ mpy_csv.py
@@ -0,0 +1,48 @@
+﻿#
+# Class to handle CSV files
+#
+
+
+import os
+import io
+import csv
+
+class Csv:
+    """ Class to handle custom CSV interactions """
+
+    @staticmethod
+    def csv_to_array(csv_in, encoding="utf-8-sig", remove_point_zero=False, replace_substrings=None):
+        """
+        Convert csv file or csv string to a 2d array
+        csv_in -- str(path to file) or str(csv string)
+        remove_point_zero -- bool. convert decimal 71.0 to 71 only #<-- todo : link jira to this discussion
+        replace_substrings  -- [['text0', 'text'], ]
+        """
+
+        table = []
+        table_string = csv_in
+        
+        # convert file to string
+        if os.path.isfile(table_string):
+            with open(table_string, "r", encoding=encoding) as f:
+                table_string = f.read()
+                
+        # clean input text if replacement substrings defined 
+        if replace_substrings is not None:
+            for sub in replace_substrings:
+                table_string = table_string.replace(sub[0], sub[1])
+                
+        # convert string to csv
+        f = io.StringIO(table_string)  # csv reader requires file object
+        table = [x for x in csv.reader(f)]
+
+        # data = [[c.replace('\ufeff', '') for c in row] for row in reader]
+            
+        # remove ending ".0" if requested
+        if remove_point_zero:
+            for row in range(len(table)):
+                for col in range(len(table[row])):
+                    if table[row][col].endswith(".0"):
+                        table[row][col] = table[row][col][:-2]
+
+        return table
--- randomNumberGuess.py
+++ randomNumberGuess.py
@@ -1,20 +1,21 @@
 import random
-# import tkinter as tk
 from tkinter import *
 
-def secretnumber(mySn):
-    num = random.randint(1,mySn)
+
+def secretnumber(mysn):
+    num = random.randint(1, mysn)
     return num
 
+
 if __name__ == '__main__':
-    root = Tk()
-    root.title('bob')
-    root.geometry('500x500')
-    frame1 = Frame(root, bg='green')
-    frame1.pack(expand=True, fill=BOTH)
-    btn1 = Button(frame1, text = "Action", command = root.destroy)
-    btn1.pack(side=LEFT)
-    root.mainloop()
+    # root = Tk()
+    # root.title('bob')
+    # root.geometry('500x500')
+    # frame1 = Frame(root, bg='green')
+    # frame1.pack(expand=True, fill=BOTH)
+    # btn1 = Button(frame1, text="Action", command=root.destroy)
+    # btn1.pack(side=LEFT)
+    # root.mainloop()
 
     mySn = int(input("enter the range for the random number: "))
     sn = secretnumber(mySn)
@@ -22,14 +23,14 @@ if __name__ == '__main__':
     # print(sn) used to know the value
     myguess = int(input("please guess the number: "))
     for i in range(1, goes):
-        if (myguess < sn):
-            print("too low you have {} goes left".format(goes - i-1))
-        elif (myguess > sn):
-            print("too high you have {} goes left".format(goes - i-1))
-        elif (myguess == sn):
+        if myguess < sn:
+            print("too low you have {} goes left".format(goes - i - 1))
+        elif myguess > sn:
+            print("too high you have {} goes left".format(goes - i - 1))
+        elif myguess == sn:
             print("perfect")
             exit()
-        i = i+1
+        i = i + 1
         if i != goes:
             myguess = int(input("please another guess at the number: "))
     print("Sorry you had too many goes")
