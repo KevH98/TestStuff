2c236e771a12b395873310918292aeca57d2589d
KevH98 <har_kev@hotmail.com>
2022-05-05 09:20:22 +0100

    updated to real csv

--- employee_training.py
+++ employee_training.py
@@ -3,12 +3,12 @@ import mpy_csv
 def get_training_procedures(assigned_roles, employee_name):
     procs = []
     unique_procs = []
-    csvfile = mpy_csv.Csv.csv_to_array("/Users/kevinharris/PycharmProjects/EmployeeProject/Book1.csv")
-    print(csvfile[0][1])
+    csvfile = mpy_csv.Csv.csv_to_array("./Book1.csv")
+    # print(csvfile[0][1])
     for i in range(len(csvfile)):
         for role in assigned_roles:
-            if role in csvfile[i][2]:
-                procs.append(csvfile[i][0])
+            if role in csvfile[i][6]:
+                procs.append(csvfile[i][1])
 
     for word in procs:
         if word not in unique_procs:
@@ -17,9 +17,9 @@ def get_training_procedures(assigned_roles, employee_name):
     print(f'The procedures to be trained for {employee_name} are: {unique_procs}')
 
 def add_roles(assigned_roles):
-    role = str(input('add roles or "q" to quit adding and get the procuders list: '))
+    role = str(input('add roles or "q" to quit adding and get the procedures list: '))
     if (role == 'q' or role == 'Q'):
-        print(f"bye the roles are: {assigned_roles}")
+        print(f"goodbye the roles filtered for are: {assigned_roles}")
         return assigned_roles
     elif (role != 'q' or role != 'Q'):
         assigned_roles.append(role)
